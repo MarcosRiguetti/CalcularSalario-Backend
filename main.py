4fa6777cadac2ea49f1aafe5fa01a2bbcbd42a2f
Marcos Riguetti <marcos_riguetti_soares@outlook.com>
2021-11-20 15:11:11 -0300

    add restante de classe vendedor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 
+from vendedor import Vendedor
+
 api = Flask(__name__)
 
 if __name__ == "__main__":
@@ -12,3 +14,12 @@ def ola_mundo():
 @api.route('/api/hello_word')
 def hello_word():
     return "Hello Word"
+
+@api.route('/cpi/calcular_valor_receber/<nome>/<meses_contrato>/<valor_venda>', methods=['GET'])
+def calcular_valor_receber(nome, meses_contrato, valor_venda):
+    vendedor = Vendedor(nome, int(meses_contrato), float(valor_venda))
+    perc_comissao = Vendedor.calcular_comissao()
+    salario_base = Vendedor.calcular_salario_base()
+    valor_receber = Vendedor.calcular_valor_receber()
+
+    return jsonify({'perc_comissao': perc_comissao, 'salario_base': salario_base, 'valor_receber': valor_receber})
